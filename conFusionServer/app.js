dc0ffb122d0bea0786a54613b7615c6b35993806
Shane Honanie <shaneh425@gmail.com>
2018-11-10 16:21:11 -0800

    Passport JWT

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -8,6 +8,7 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -19,7 +20,7 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -38,32 +39,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-	name: 'session-id',
-	secret: '12345-67890-09876-54321',
-	saveUninitialized: false,
-	resave: false,
-	store: new FileStore()
-}));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
 
-function auth (req, res, next) {
-	if(!req.user) {
-		var err = new Error('You are not authenticated!');
-		err.status = 403;
-		return next(err);
-	}
-	else
-		next();
-}
-
-app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
